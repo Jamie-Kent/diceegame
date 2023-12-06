d8ae85d74a2a1e659c82350cede496cc5f9b18e2
Jamie Kent <55166609+Jamie-Kent@users.noreply.github.com>
2023-12-06 20:22:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,112 @@
+// DICE 1 GENERATOR
+    var diceNumber1 = Math.ceil(Math.random() * 6);
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+// DICE2 GENERATOR
+    var diceNumber2 = Math.ceil(Math.random() * 6);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    
+// DICE 1 IMAGE SOURCE
+    var randomImageSource1 = "images/dice" + diceNumber1 + ".png";
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+    document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
 
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+
+// DICE 2 IMAGE SOURCE
+    var randomImageSource2 = "images/dice" + diceNumber2 + ".png"; 
+
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// WIN LOGIC
+    if (diceNumber1 > diceNumber2) {
+        document.querySelector("div h1").textContent = "🚩 Player 1 Wins";
+    }
+    else if (diceNumber1 < diceNumber2) {
+        document.querySelector("div h1").textContent = "Player 2 Wins 🚩";
+    }
+    else {
+        document.querySelector("div h1").textContent = "Draw!";
+    }
+
+
+
+// function diceeGame () {
+
+//     // DICE1
+//     // var rand1 = Math.random();
+//     // diceNumber1 = Math.ceil(rand1 * 6)
+//     //  console.log(diceNumber1)
+
+//     var diceNumber1 = Math.ceil(Math.random() * 6);
+
+//     // DICE2
+//     var diceNumber2 = Math.ceil(Math.random() * 6);
+
+
+//     // PLAYER 1 DICE IMAGES
+//     if (diceNumber1 == 1) {
+//         console.log("Player 1 scored 1")
+//         document.querySelector("img.img1").setAttribute("src", "images/dice1.png")
+//     }
+//     else if (diceNumber1 == 2) {
+//         console.log("Player 1 scored 2") 
+//         document.querySelector("img.img1").setAttribute("src", "images/dice2.png")
+//     }
+//     else if (diceNumber1 == 3) {
+//         console.log("Player 1 scored 3") 
+//         document.querySelector("img.img1").setAttribute("src", "images/dice3.png")
+//     }
+//     else if (diceNumber1 == 4) {
+//         console.log("Player 1 scored 4") 
+//         document.querySelector("img.img1").setAttribute("src", "images/dice4.png")
+//     }
+//     else if (diceNumber1 == 5) {
+//         console.log("Player 1 scored 5") 
+//         document.querySelector("img.img1").setAttribute("src", "images/dice5.png")
+//     }
+//     else if (diceNumber1 == 6) {
+//         console.log("Player 1 scored 6") 
+//         document.querySelector("img.img1").setAttribute("src", "images/dice6.png")
+//     }
+
+//     // PLAYER 2 DICE IMAGES
+//     if (diceNumber2 == 1) {
+//         console.log("Player 2 scored 1")
+//         document.querySelector("img.img2").setAttribute("src", "images/dice1.png")
+//     }
+//     else if (diceNumber2 == 2) {
+//         console.log("Player 2 scored 2") 
+//         document.querySelector("img.img2").setAttribute("src", "images/dice2.png")
+//     }
+//     else if (diceNumber2 == 3) {
+//         console.log("Player 2 scored 3") 
+//         document.querySelector("img.img2").setAttribute("src", "images/dice3.png")
+//     }
+//     else if (diceNumber2 == 4) {
+//         console.log("Player 2 scored 4") 
+//         document.querySelector("img.img2").setAttribute("src", "images/dice4.png")
+//     }
+//     else if (diceNumber2 == 5) {
+//         console.log("Player 2 scored 5") 
+//         document.querySelector("img.img2").setAttribute("src", "images/dice5.png")
+//     }
+//     else if (diceNumber2 == 6) {
+//         console.log("Player 2 scored 2") 
+//         document.querySelector("img.img2").setAttribute("src", "images/dice6.png")
+//     }
+
+    
+
+//     if (diceNumber1 > diceNumber2) {
+//         document.querySelector("div h1").textContent = "🚩 Player 1 Wins";
+//     }
+//     else if (diceNumber1 < diceNumber2) {
+//         document.querySelector("div h1").textContent = "Player 2 Wins 🚩";
+//     }
+//     else {
+//         document.querySelector("div h1").textContent = "Draw!";
+//     }
+
+// }
